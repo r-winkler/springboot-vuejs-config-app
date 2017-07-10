274f2746e5fa298cccacc9445b0327a0ec9d6e87
René Winkler <rene.winkler@xovis.com>
2017-07-10 18:50:16 +0200

    Edit vue.js

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -1,7 +1,31 @@
-var main = new Vue({
-    el: '#module',
-    data: {
-        message: 'Hello Vue.js!'
+var database = new Vue({
+        el: '#database',
+        data: {
+            databaseConfiguration: {
+                type: '',
+                url: '',
+                port: '',
+                username: '',
+                password: ''
+            },
+            errors: []
+        },
+        created: function () {
+            axios.get('/config/database')
+                .then(function (response) {
+                    this.databaseConfiguration = response.data;
+                    console.log(response.data);
+                })
+                .catch(function (error) {
+                    this.errors.push(error);
+                    console.log(error);
+                })
+        }
+
     }
-})
+)
+
+
+
+
 
